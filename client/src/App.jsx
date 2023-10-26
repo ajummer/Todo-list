a0c7024f3c69c16a3feae37cb278b53830323c92
ajummer <makeudreamempty@gmail.com>
2023-10-26 19:49:39 +0300

    Add toggle status

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,10 +8,15 @@ function App() {
   useEffect(() => {
     fetch("http://localhost:3030/jsonstore/todos").then((response) =>
       response.json().then((data) => {
-        setTodos(Object.values(data));
+        const result = Object.keys(data).map((id) => ({ id, ...data[id] }));
+        setTodos(result);
       })
     );
   }, []);
+ 
+  const toggleStatus = (id) => {
+    setTodos(state => state.map(todo => todo.id === id ? {...todo, isCompleted: !todo.isCompleted} : todo))
+  };
 
   return (
     <div>
@@ -20,7 +25,6 @@ function App() {
       <main className="main">
         <section className="todo-list-container">
           <h1>Todo List</h1>
-
           <div className="add-btn-container">
             <button className="btn">+ Add new Todo</button>
           </div>
@@ -34,7 +38,7 @@ function App() {
                   <th className="table-header-action">Action</th>
                 </tr>
               </thead>
-              <TodoList todos={todos} />
+              <TodoList todos={todos} toggleStatus={toggleStatus} />
             </table>
           </div>
         </section>
--- client/src/components/TodoList.jsx
+++ client/src/components/TodoList.jsx
@@ -1,12 +1,15 @@
-export default function TodoList({ todos }) {
+export default function TodoList({ 
+    todos,
+    toggleStatus
+ }) {
   return (
     <tbody>
      {todos.map(todo => (
-          <tr className={todo.isCompleted ? "todo is-completed" : "todo"}>
+          <tr key={todo.id} className={todo.isCompleted ? "todo is-completed" : "todo"} >
           <td>{todo.text}</td>
           <td>{todo.isCompleted ? "Completed" : "Not completed"}</td>
           <td className="todo-action">
-            <button className="btn todo-btn">Change status</button>
+            <button className="btn todo-btn" onClick={() => toggleStatus(todo.id)}>Change status</button>
           </td>
         </tr>
      ))}
